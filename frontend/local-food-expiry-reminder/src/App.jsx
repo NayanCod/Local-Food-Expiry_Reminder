881e1b26f65751812561902b595cfb94397be193
Milan Singh <milan083821@gmail.com>
2024-11-16 16:53:39 +0530

    initialized frontend

---
+++ frontend/local-food-expiry-reminder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-black min-h-screen text-white text-center'>
+      <h1>Local Food Expiry Reminder</h1>
+    </div>
+  )
+}
+
+export default App
