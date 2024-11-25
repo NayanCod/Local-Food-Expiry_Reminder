48e61fa7f34105352e8719796ae464143325ffe3
NayanCod <kumarnayab6@gmail.com>
2024-11-25 16:20:00 +0530

    created axios interceptor file to avoid repeatition

--- frontend/local-food-expiry-reminder/axiosConfig.js
+++ frontend/local-food-expiry-reminder/axiosConfig.js
@@ -1,21 +1,18 @@
 import axios from 'axios';
 
-// Create an instance of axios
+
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080', // Set your base URL
+  baseURL: 'http://localhost:8080', 
   headers: {
-    'Content-Type': 'application/json', // Default Content-Type for all requests
+    'Content-Type': 'application/json',
   },
 });
 
-// Add a request interceptor to automatically add the Authorization header
 axiosClient.interceptors.request.use(
   (config) => {
-    // You can retrieve the token from localStorage or any other place
     const token = localStorage.getItem('token');
     
     if (token) {
-      // Add the Authorization header if the token exists
       config.headers['Authorization'] = `Bearer ${token}`;
     }
 
