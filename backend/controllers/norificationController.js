d732a1e2590f82f07401b9a5c701fa6b4e0c07a3
NayanCod <kumarnayab6@gmail.com>
2024-11-25 16:14:19 +0530

    created get and update notification endpoint

--- backend/controllers/norificationController.js
+++ backend/controllers/norificationController.js
@@ -3,12 +3,16 @@ const Notify = require("../model/Notify");
 exports.addNotification = async (req, res) => {
   const { title, message, timestamp } = req.body;
   try {
+    const user = req.user.id;
+    if (!user) {
+      return res.status(500).send("User not found!");
+    }
     const notifcation = new Notify({
       title: title,
       message: message,
       timestamp: timestamp,
       isRead: false,
-      user: req.user.id,
+      user: user,
     });
     await notifcation.save();
     console.log("Notifcation saved successfully");
@@ -35,33 +39,26 @@ exports.getNotifications = async (req, res) => {
 };
 
 exports.updateNotification = async (req, res) => {
-    const { id } = req.params;
-    try {
-      // Check if the user exists
-      if (!req.user.id) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Find the notification by user ID and notification ID
-      const notification = await Notify.findOne({ user: req.user.id, _id: id });
-  
-      if (!notification) {
-        return res.status(404).json({ message: 'Notification not found' });
-      }
-  
-      // Update the notification's isRead field to true
-      notification.isRead = true;
-      const updatedNotification = await notification.save(); // Save the updated notification
-  
-      // Return the updated notification in the response
-      return res.status(200).json({
-        message: 'Notification updated successfully',
-        updatedNotification, // Send the updated notification object
-      });
-  
-    } catch (error) {
-      console.error('Error updating notification:', error);
-      return res.status(500).json({ message: 'Failed to update notification' });
+  const { id } = req.params;
+  try {
+    if (!req.user.id) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
-  
+
+    const notification = await Notify.findOne({ user: req.user.id, _id: id });
+
+    if (!notification) {
+      return res.status(404).json({ message: "Notification not found" });
+    }
+
+    notification.isRead = true;
+    const updatedNotification = await notification.save();
+
+    return res.status(200).json({
+      message: "Notification updated successfully",
+    });
+  } catch (error) {
+    console.error("Error updating notification:", error);
+    return res.status(500).json({ message: "Failed to update notification" });
+  }
+};
