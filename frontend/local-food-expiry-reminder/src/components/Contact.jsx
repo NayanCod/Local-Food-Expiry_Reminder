ef46ce36f90024d2eeefcca4e69e7269bd02d65b
NayanCod <kumarnayab6@gmail.com>
2024-12-17 14:51:04 +0530

    landing page style changes

--- frontend/local-food-expiry-reminder/src/components/Contact.jsx
+++ frontend/local-food-expiry-reminder/src/components/Contact.jsx
@@ -9,6 +9,7 @@ function Contact() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [details, setDetails] = useState("");
   const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
   const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
   const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
   const userID = import.meta.env.VITE_EMAILJS_USER_ID;
@@ -19,17 +20,21 @@ function Contact() {
     if (!email) errors.email = "Email is required";
     else if (!/\S+@\S+\.\S+/.test(email)) errors.email = "Email is invalid";
     if (!phoneNumber) errors.phoneNumber = "Phone number is required";
-    else if (!/^\d{10}$/.test(phoneNumber)) errors.phoneNumber = "Phone number must be 10 digits";
+    else if (!/^\d{10}$/.test(phoneNumber))
+      errors.phoneNumber = "Phone number must be 10 digits";
     if (!details) errors.details = "Details are required";
 
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!validate()) return;
+
+    setLoading(true);
+    console.log("loading true");
 
     const formData = {
       firstName,
@@ -40,22 +45,42 @@ function Contact() {
     };
 
     // Using EmailJS to send the email
-    emailjs
-      .send(serviceId, templateId, formData, userID)
-      .then(
-        (response) => {
-          toast.success("Your inquiry has been sent!");
-          // Reset the form fields
-          setFirstName("");
-          setLastName("");
-          setEmail("");
-          setPhoneNumber("");
-          setDetails("");
-        },
-        (err) => {
-          toast.error("Something went wrong. Please try again.");
-        }
-      );
+    const sendEmail = await emailjs.send(
+      serviceId,
+      templateId,
+      formData,
+      userID
+    );
+    if (sendEmail) {
+      toast.success("Your inquiry has been sent!");
+      // Reset the form fields
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPhoneNumber("");
+      setDetails("");
+      setLoading(false);
+    } else {
+      toast.error("Something went wrong. Please try again.");
+      setLoading(false);
+    }
+    // emailjs.send(serviceId, templateId, formData, userID).then(
+    //   (response) => {
+    //     toast.success("Your inquiry has been sent!");
+    //     // Reset the form fields
+    //     setFirstName("");
+    //     setLastName("");
+    //     setEmail("");
+    //     setPhoneNumber("");
+    //     setDetails("");
+    //   },
+    //   (err) => {
+    //     toast.error("Something went wrong. Please try again.");
+    //     setLoading(false);
+    //   }
+    // );
+    // setLoading(false);
+    // console.log("loading false");
   };
 
   return (
@@ -97,7 +122,9 @@ function Contact() {
                       type="text"
                       name="firstName"
                       id="hs-firstname-contacts-1"
-                      className={`py-3 px-4 block w-full border ${errors.firstName ? "border-red-500" : "border-gray-400"} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
+                      className={`py-3 px-4 block w-full border ${
+                        errors.firstName ? "border-red-500" : "border-gray-400"
+                      } rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
                       value={firstName}
                       onChange={(e) => setFirstName(e.target.value)}
                     />
@@ -118,7 +145,9 @@ function Contact() {
                       type="text"
                       name="lastName"
                       id="hs-lastname-contacts-1"
-                      className={`py-3 px-4 block w-full border ${errors.lastName ? "border-red-500" : "border-gray-400"} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
+                      className={`py-3 px-4 block w-full border ${
+                        errors.lastName ? "border-red-500" : "border-gray-400"
+                      } rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
                       value={lastName}
                       onChange={(e) => setLastName(e.target.value)}
                     />
@@ -142,7 +171,9 @@ function Contact() {
                       name="email"
                       id="hs-email-contacts-1"
                       autoComplete="email"
-                      className={`py-3 px-4 block w-full border ${errors.email ? "border-red-500" : "border-gray-400"} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
+                      className={`py-3 px-4 block w-full border ${
+                        errors.email ? "border-red-500" : "border-gray-400"
+                      } rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                     />
@@ -163,12 +194,18 @@ function Contact() {
                       type="text"
                       name="phoneNumber"
                       id="hs-phone-number-1"
-                      className={`py-3 px-4 block w-full border ${errors.phoneNumber ? "border-red-500" : "border-gray-400"} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
+                      className={`py-3 px-4 block w-full border ${
+                        errors.phoneNumber
+                          ? "border-red-500"
+                          : "border-gray-400"
+                      } rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
                       value={phoneNumber}
                       onChange={(e) => setPhoneNumber(e.target.value)}
                     />
                     {errors.phoneNumber && (
-                      <p className="text-sm text-red-500">{errors.phoneNumber}</p>
+                      <p className="text-sm text-red-500">
+                        {errors.phoneNumber}
+                      </p>
                     )}
                   </div>
                 </div>
@@ -185,7 +222,9 @@ function Contact() {
                     id="hs-about-contacts-1"
                     name="details"
                     rows="4"
-                    className={`py-3 px-4 block w-full border ${errors.details ? "border-red-500" : "border-gray-400"} rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
+                    className={`py-3 px-4 block w-full border ${
+                      errors.details ? "border-red-500" : "border-gray-400"
+                    } rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500`}
                     value={details}
                     onChange={(e) => setDetails(e.target.value)}
                   ></textarea>
@@ -199,9 +238,52 @@ function Contact() {
               <div className="mt-6">
                 <button
                   type="submit"
-                  className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 transition"
+                  className={`w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg ${
+                    loading
+                      ? "bg-blue-300 cursor-not-allowed"
+                      : "bg-blue-600 hover:bg-blue-700"
+                  } text-white`}
+                  disabled={loading}
                 >
-                  Send inquiry
+                  {loading ? (
+                    <div className="flex items-center gap-x-2">
+                    Send inquiry
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-4 h-4 animate-spin"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                    >
+                      <circle
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        strokeWidth="4"
+                        stroke="currentColor"
+                        fill="none"
+                      />
+                      <path
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                        d="M4 12a8 8 0 0 1 16 0"
+                      />
+                    </svg>
+                    </div>
+                  ) : (
+                    <div className="flex items-center gap-x-2">
+                      Send inquiry
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="m21.426 11.095-17-8A1 1 0 0 0 3.03 4.242l1.212 4.849L12 12l-7.758 2.909-1.212 4.849a.998.998 0 0 0 1.396 1.147l17-8a1 1 0 0 0 0-1.81z"></path>
+                      </svg>
+                    </div>
+                  )}
                 </button>
               </div>
 
--- frontend/local-food-expiry-reminder/src/components/Hero.jsx
+++ frontend/local-food-expiry-reminder/src/components/Hero.jsx
@@ -8,14 +8,14 @@ function Hero() {
     {/* Hero Heading */}
     <div className="max-w-3xl text-center mx-auto">
       <h1 className="font-extrabold text-gray-100 text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-tight">
-        Never Let Your Item <br /> Go to Waste
+        Never Let Your Item <br /> Go to <span className="text-green-400">Waste</span>
       </h1>
     </div>
 
     {/* Hero Description */}
     <div className="max-w-3xl text-center mx-auto">
       <p className="text-lg text-white/80 leading-relaxed">
-        Introducing <strong className="text-white">FreshTrack</strong>, your smart companion for managing items expiry dates. 
+        Introducing <strong className="text-green-400">FreshTrack</strong>, your smart companion for managing items expiry dates. 
         Track expiry dates, and get timely reminders to ensure nothing goes bad. 
         Simplify your kitchen and reduce waste effortlessly with our intuitive app.
       </p>
@@ -25,7 +25,7 @@ function Hero() {
     <div className="text-center">
       <Link
         to={isAuthenticated ? "/home" : "/login"}
-        className="inline-flex items-center gap-x-3 bg-white text-blue-600 text-sm font-semibold shadow-md rounded-full px-6 py-3 hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 transition duration-300"
+        className="inline-flex items-center gap-x-3 bg-white text-blue-600 text-sm font-semibold hover:shadow-lg rounded-full px-6 py-3 hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 transition duration-300"
       >
         Get started
         <svg
